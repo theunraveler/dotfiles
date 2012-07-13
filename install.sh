7f62d23d86f8559fce9634d4d434966216197485
Jake Bell <jake@theunraveler.com>
2012-07-12 19:39:22 -0500

    Adding git submodule stuff to install script.

--- install.sh
+++ install.sh
@@ -3,22 +3,29 @@
 WD="$(pwd)"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# First, initialize git submodules.
+git submodule init; git submodule update;
+
+# Then, symlink all dotfiles into the home directory.
 cd ~;
-ln -s $DIR/bin .bin;
-ln -s $DIR/cap/caprc .caprc;
-ln -s $DIR/drush/drush .drush;
-ln -s $DIR/git/gitconfig .gitconfig;
-ln -s $DIR/git/gitignore .gitignore;
-ln -s $DIR/git/template .gittemplate;
-ln -s $DIR/htop/htoprc .htoprc;
-ln -s $DIR/general/hushlogin .hushlogin;
-ln -s $DIR/mysql/my.cnf .my.cnf;
-ln -s $DIR/node/npmrc .npmrc;
-ln -s $DIR/rsync .rsync;
-ln -s $DIR/rvm/rvmrc .rvmrc;
-ln -s $DIR/tmux/tmux.conf .tmux.conf;
-ln -s $DIR/vim/vim .vim;
-ln -s $DIR/vim/vimrc .vimrc;
-ln -s $DIR/zsh/zsh .zsh;
-ln -s $DIR/zsh/zshrc .zshrc;
-cd $WD;
+ln -s $DIR/bin .bin
+ln -s $DIR/cap/caprc .caprc
+ln -s $DIR/drush/drush .drush
+ln -s $DIR/git/gitconfig .gitconfig
+ln -s $DIR/git/gitignore .gitignore
+ln -s $DIR/git/template .gittemplate
+ln -s $DIR/htop/htoprc .htoprc
+ln -s $DIR/general/hushlogin .hushlogin
+ln -s $DIR/mysql/my.cnf .my.cnf
+ln -s $DIR/node/npmrc .npmrc
+ln -s $DIR/rsync .rsync
+ln -s $DIR/rvm/rvmrc .rvmrc
+ln -s $DIR/tmux/tmux.conf .tmux.conf
+ln -s $DIR/vim/vim .vim
+ln -s $DIR/vim/vimrc .vimrc
+ln -s $DIR/zsh/zsh .zsh
+ln -s $DIR/zsh/zshrc .zshrc
+
+# Finally, go back to where the user started.
+cd $WD
+exit 0
