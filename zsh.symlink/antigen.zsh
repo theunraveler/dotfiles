1fd563c813ec9c94d6d1b2730c66335ea4ac6506
Jake Bell <jake@theunraveler.com>
2014-09-19 17:58:21 -0500

    Adding some new zsh plugins

--- zsh.symlink/antigen.zsh
+++ zsh.symlink/antigen.zsh
@@ -2,11 +2,13 @@ source $HOME/.zsh/antigen/antigen.zsh
 antigen use oh-my-zsh
 antigen bundles <<EOBUNDLES
   brew
+  bundler
   composer
   extract
   git
   git-flow
   gitignore
+  postgres
   terminalapp
 EOBUNDLES
 antigen theme theunraveler
