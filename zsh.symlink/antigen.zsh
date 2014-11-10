db2cb05c67b543e94b2dd9804a8bc05f7c10f6d8
Jake Bell <jake@theunraveler.com>
2014-11-09 18:37:35 -0600

    Adding some new plugins and packages

--- zsh.symlink/antigen.zsh
+++ zsh.symlink/antigen.zsh
@@ -3,13 +3,20 @@ antigen use oh-my-zsh
 antigen bundles <<EOBUNDLES
   brew
   bundler
+  chruby
+  colored-man
   composer
   extract
   git
   git-flow
+  github
   gitignore
   postgres
+  rake
+  rake-fast
   terminalapp
+  zsh_reload
+  zsh-users/zsh-syntax-highlighting
 EOBUNDLES
 antigen theme theunraveler
 antigen apply
