fb7019168fada9ed7baf8808419bee10d52c903f
Jake Bell <jake@theunraveler.com>
2015-05-18 10:34:42 -0500

    Removing some zsh plugins

--- zsh.symlink/zgen.zsh
+++ zsh.symlink/zgen.zsh
@@ -1,30 +1,27 @@
 source $HOME/.zsh/zgen/zgen.zsh
 
 if ! zgen saved; then
-    echo "Creating a zgen save"
-    zgen oh-my-zsh
+  echo "Creating a zgen save"
+  zgen oh-my-zsh
 
-    zgen oh-my-zsh plugins/brew
-    zgen oh-my-zsh plugins/bundler
-    zgen oh-my-zsh plugins/chruby
-    zgen oh-my-zsh plugins/colored-man
-    zgen oh-my-zsh plugins/extract
-    zgen oh-my-zsh plugins/git
-    zgen oh-my-zsh plugins/git-flow
-    zgen oh-my-zsh plugins/gitignore
-    zgen oh-my-zsh plugins/osx
-    zgen oh-my-zsh plugins/pj
-    zgen oh-my-zsh plugins/rake
-    zgen oh-my-zsh plugins/sudo
-    zgen oh-my-zsh plugins/terminalapp
-    zgen oh-my-zsh plugins/zsh_reload
+  zgen oh-my-zsh plugins/brew
+  zgen oh-my-zsh plugins/bundler
+  zgen oh-my-zsh plugins/chruby
+  zgen oh-my-zsh plugins/colored-man
+  zgen oh-my-zsh plugins/extract
+  zgen oh-my-zsh plugins/git
+  zgen oh-my-zsh plugins/git-flow
+  zgen oh-my-zsh plugins/osx
+  zgen oh-my-zsh plugins/pj
+  zgen oh-my-zsh plugins/terminalapp
+  zgen oh-my-zsh plugins/zsh_reload
 
-    zgen load djui/alias-tips
-    zgen load zsh-users/zsh-syntax-highlighting
+  zgen load djui/alias-tips
+  zgen load zsh-users/zsh-syntax-highlighting
 
-    zgen load zsh-users/zsh-completions src
+  zgen load zsh-users/zsh-completions src
 
-    zgen oh-my-zsh themes/theunraveler
+  zgen oh-my-zsh themes/theunraveler
 
-    zgen save
+  zgen save
 fi
