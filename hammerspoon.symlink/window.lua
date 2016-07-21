dc6343681b672465141b687b065ae55014bca855
Jake Bell <jake@theunraveler.com>
2016-07-21 16:25:11 -0500

    hammerspoon: Moving window stuff to utility function

--- hammerspoon.symlink/window.lua
+++ hammerspoon.symlink/window.lua
@@ -2,32 +2,92 @@
 -- WINDOW MANAGEMENT
 --
 
--- Left half.
-hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+function windowResize(x, y, w, h)
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
+  f.x = x and x(max) or max.x
+  f.y = y and y(max) or max.y
+  f.w = w and w(max) or max.w
+  f.h = h and h(max) or max.h
   win:setFrame(f)
+end
+
+-- Top half.
+hs.hotkey.bind({"cmd", "alt"}, "Up", function()
+  windowResize(nil, nil, nil, function(m)
+    return m.h / 2
+  end)
+end)
+
+-- Bottom half.
+hs.hotkey.bind({"cmd", "alt"}, "Down", function()
+  windowResize(
+    nil,
+    function(m) return m.h / 2 end,
+    nil,
+    function(m) return m.h / 2 end
+  )
+end)
+
+-- Left half.
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+  windowResize(
+    nil,
+    nil,
+    function(m) return m.w / 2 end
+  )
 end)
 
 -- Right half.
 hs.hotkey.bind({"cmd", "alt"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+  windowResize(
+    function(m) return m.w / 2 end,
+    nil,
+    function(m) return m.w / 2 end
+  )
+end)
 
-  f.x = max.w / 2
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+-- Upper left.
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  windowResize(
+    nil,
+    nil,
+    function(m) return m.w / 2 end,
+    function(m) return m.h / 2 end
+  )
+end)
+
+-- Lower left.
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  windowResize(
+    nil,
+    function(m) return m.h / 2 end,
+    function(m) return m.w / 2 end,
+    function(m) return m.h / 2 end
+  )
+end)
+
+-- Upper right.
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  windowResize(
+    function(m) return m.w / 2 end,
+    nil,
+    function(m) return m.w / 2 end,
+    function(m) return m.h / 2 end
+  )
+end)
+
+-- Lower right.
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  windowResize(
+    function(m) return m.w / 2 end,
+    function(m) return m.h / 2 end,
+    function(m) return m.w / 2 end,
+    function(m) return m.h / 2 end
+  )
 end)
 
 -- Full screen.
