3a7184974dfb67e91b25ec53b82b224993075be7
Jake Bell <jake@theunraveler.com>
2017-05-19 10:32:12 -0500

    Bash styleguide changes

--- install.sh
+++ install.sh
@@ -4,26 +4,23 @@ WD="$(pwd)"
 DIR="$( cd "$(dirname "$0")/.." && pwd )"
 BREW_CMD="/usr/local/bin/brew"
 
-cd ~
+cd ~ || exit 1
 echo "Linking items from $DIR to home directory using $(which ln)"
 
 # If we are using GNU ln (from coreutils) the options are different.
-ln --version > /dev/null
-if [ $? -eq 0 ]; then
+if ln --version > /dev/null; then
   ln_command="ln -ns"
 else
   ln_command="ln -hs"
 fi
 
 # Then, symlink all dotfiles into the home directory.
-for filepath in `find "$DIR" ! -path "*.git*" -name "*.symlink"`; do
+while IFS= read -r -d '' filepath; do
   filename=$(basename "${filepath%.*}")
   echo -n "Linking $filename..."
 
-  $ln_command "$filepath" ".$filename"
-
-  if [ $? -eq 0 ]; then echo "done"; fi
-done
+  if $ln_command "$filepath" ".$filename"; then echo "done"; fi
+done <   <(find "$DIR" ! -path '*.git*' -name '*.symlink' -print0)
 
 # Install homebrew if we need to.
 if [ ! -f $BREW_CMD ]; then
@@ -37,17 +34,21 @@ brew bundle --global
 echo "done."
 
 # Install zgen.
-cd ~/.zsh
+cd ~/.zsh || exit 1
 git clone https://github.com/tarjoilija/zgen.git zgen
-cd -
+cd - || exit 1
 
 # Install vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
 # Install composer packages.
-which composer && cd .composer && composer --dev install && cd -;
+if which composer > /dev/null; then
+  cd .composer || exit 1
+  composer --dev install
+  cd - || exit 1
+fi
 
 # Finally, go back to where the user started.
-cd "$WD"
+cd "$WD" || exit 1
 exit 0
