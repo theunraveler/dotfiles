fe6f72a70014b370750a9bdb00c31836739ef5f3
Jake Bell <jake@theunraveler.com>
2017-10-25 12:54:56 -0500

    Reverting 4391fcad7137573b142b5e3c61309f8c1cdad9aa, spoons are unreliable

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,5 +1,18 @@
 require "_config"
-require 'spoons'
 utils = require "utils"
 require "itunes"
 require "spotify"
+
+--- Reload hammerspoon config whenever one of its config files changes.
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+    utils.notify("Config reloaded")
+  end
+end):start()
