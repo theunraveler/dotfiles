6c10da807280daeb2d01924beb0e0af0d54529d4
Jake Bell <sigterm01@gmail.com>
2017-11-16 10:56:23 -0600

    Moving back to prezto history-substring-search

--- zsh/.zsh/zplug.zsh
+++ zsh/.zsh/zplug.zsh
@@ -5,17 +5,20 @@ zplug "djui/alias-tips"
 zplug "Tarrasch/zsh-autoenv"
 zplug "theunraveler/zsh-fancy_ctrl_z"
 zplug "modules/gnu-utility", from:prezto
+zplug "modules/history-substring-search", from:prezto
 zplug "modules/ruby", from:prezto
 zplug "modules/utility", from:prezto
 zplug "zimframework/archive"
 zplug "zimframework/history"
 zplug "zimframework/prompt"
 zplug "zsh-users/zsh-completions", use:src
-zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-zstyle ':prezto:module:ruby:chruby' auto-switch 'yes'
 zstyle ':prezto:module:*:*' color 'yes'
+zstyle ':prezto:module:history-substring-search' color 'no'
+zstyle ':prezto:module:history-substring-search' globbing-flags ''
+zstyle ':prezto:module:ruby:chruby' auto-switch 'yes'
+
 zprompt_theme='theunraveler'
 
 # Install plugins if there are plugins that have not been installed
