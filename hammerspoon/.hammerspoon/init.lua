ce515cb2e169ed05ffdc4ef63fe2e8837e58a1ed
Jake Bell <sigterm01@gmail.com>
2020-09-30 16:32:23 -0500

    Simplifying hammerspoon config a bit

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,19 +1,15 @@
 require "_config"
 utils = require "utils"
-require "deadman"
 require "vpn"
 require "window"
 
 --- Reload hammerspoon config whenever one of its config files changes.
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
-  doReload = false
   for _,file in pairs(files) do
     if file:sub(-4) == ".lua" then
-      doReload = true
+      hs.reload()
+      utils.notify("Config reloaded")
+      break
     end
-  end
-  if doReload then
-    hs.reload()
-    utils.notify("Config reloaded")
   end
 end):start()
