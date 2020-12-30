efffbd01c5febbaa54faf27a0e027090c0dbe30c
Jake Bell <sigterm01@gmail.com>
2020-12-29 18:07:23 -0600

    XDG FTW

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,10 @@
+# XDG paths.
+set --export XDG_CONFIG_HOME "$HOME"/.config
+set --export XDG_CACHE_HOME "$HOME"/.cache
+set --export XDG_DATA_HOME "$HOME"/.local/share
+set --export XDG_BIN_HOME "$HOME"/.local/bin
+set --export XDG_RUNTIME_DIR "$HOME"/.local/run
+
 set --export CLICOLOR 1
 set fish_greeting
 
@@ -5,25 +12,43 @@ set fish_greeting
 eval (brew shellenv)
 
 # Initialize run path
-set --export PATH $HOME/.bin $PATH
+set --export PATH $XDG_BIN_HOME $PATH
 
 # Default programs
 set --export BROWSER 'open'
 set --export EDITOR 'mvim -f'
 set --export MARKDOWN 'cmark'
 
+# less
+set --export LESSKEY "$XDG_CONFIG_HOME"/less/lesskey
+set --export LESSHISTFILE "$XDG_CACHE_HOME"/less/history
+
 # Homebrew
 set --export HOMEBREW_NO_ANALYTICS 1
 
 # Python.
 set --export PIP_REQUIRE_VIRTUALENV true
-set --export PIP_CONFIG_FILE "$HOME/.pip.conf"
 set --export PYTHONDONTWRITEBYTECODE 1
 set --export PIPENV_VENV_IN_PROJECT 1
+
+# ruby
+set --export BUNDLE_USER_CONFIG "$XDG_CONFIG_HOME"/bundle
+set --export BUNDLE_USER_CACHE "$XDG_CACHE_HOME"/bundle
+set --export BUNDLE_USER_PLUGIN "$XDG_DATA_HOME"/bundle
+set --export GEMRC "$XDG_CONFIG_HOME"/gem/gemrc
+set --export GEM_HOME "$XDG_DATA_HOME"/gem
+set --export GEM_SPEC_CACHE "$XDG_CACHE_HOME"/gem
+set --export IRBRC "$XDG_CONFIG_HOME"/irb/irbrc
+
+# nodejs/npm
+set --export NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
 
 # grep
 set --export GREP_COLOR '37;45'           # BSD.
 set --export GREP_COLORS "mt=$GREP_COLOR" # GNU.
+
+# ack
+set --export ACKRC "$XDG_CONFIG_HOME/ack/ackrc"
 
 # man
 set --export MANPAGER "sh -c 'col -bx | bat -l man -p'"
@@ -48,16 +73,45 @@ set --export FZF_DEFAULT_COMMAND 'rg --files --follow --hidden'
 ssh-add -A &> /dev/null
 
 # GPG
-set -x GPG_TTY (tty)
-gpgconf --launch gpg-agent
+set --export GNUPGHOME "$XDG_DATA_HOME"/gnupg
+set --export GPG_TTY (tty)
+gpg-agent --daemon --options "$XDG_CONFIG_HOME"/gnupg/gpg-agent.conf 2>/dev/null
 
 # direnv
 direnv hook fish | source
 
 # asdf
+set --export ASDF_CONFIG_FILE "$XDG_CONFIG_HOME"/asdf/asdfrc
+set --export ASDF_DATA_DIR "$XDG_DATA_HOME"/asdf
 source $HOMEBREW_PREFIX/opt/asdf/asdf.fish
 
+# vim
+set --export VIMINIT 'if !has("nvim") | source $XDG_CONFIG_HOME/vim/vimrc | endif'
+
+# postgres
+set --export PSQLRC "$XDG_CONFIG_HOME"/postgres/psqlrc
+set --export PSQL_HISTORY "$XDG_CACHE_HOME"/postgres/history
+set --export PGPASSFILE "$XDG_CONFIG_HOME"/postgres/pass
+set --export PGSERVICEFILE "$XDG_CONFIG_HOME"/postgres/service.conf
+
+# MySQL
+set --export MYSQL_HISTFILE "$XDG_CACHE_HOME"/mysql/history
+
+# Starship
 eval (starship init fish)
+
+# aspell
+set --export ASPELL_CONF "per-conf $XDG_CONFIG_HOME/aspell/aspell.conf; personal $XDG_DATA_HOME/aspell/en.pws; repl $XDG_DATA_HOME/aspell/en.prepl"
+
+# ansible
+set --export ANSIBLE_CONFIG "$XDG_CONFIG_HOME"/ansible/ansible.cfg
+
+# aws
+set --export AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME"/aws/credentials
+set --export AWS_CONFIG_FILE "$XDG_CONFIG_HOME"/aws/config
+
+# wget
+set --export WGETRC "$XDG_CONFIG_HOME/wgetrc"
 
 # Source out to local config file.
 [ -f "$HOME/.local/share/fish/local.fish" ] && source "$HOME/.local/share/fish/local.fish"
