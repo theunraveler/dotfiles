2e7ee357458744c8841185f92a32650c9f079e7e
Jake Bell <sigterm01@gmail.com>
2021-01-11 13:54:40 -0600

    Setting psysh history directory

--- php/.config/psysh/config.php
+++ php/.config/psysh/config.php
@@ -1,5 +1,11 @@
 <?php
 
+use XdgBaseDir\Xdg;
+
+$xdg = new Xdg();
+$config->setHistoryFile($xdg->getHomeCacheDir() . '/psysh/history');
+
+
 return [
     'historySize' => 10000,
     'requireSemicolons' => true,
