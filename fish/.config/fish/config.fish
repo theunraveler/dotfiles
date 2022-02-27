3a27bbe7660636d719c340bfca99c89fc2d56e11
Jake Bell <sigterm01@gmail.com>
2022-02-27 17:58:10 -0600

    Moving fish config files to `.config/fish/conf.d`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,1 @@
 set fish_greeting
-
-source $HOME/.config/fish/env.fish
-source $HOME/.config/fish/keys.fish
-source $HOME/.config/fish/aliases.fish
-
-# Source out to local config file.
-[ -f "$HOME/.local/share/fish/local.fish" ] && source "$HOME/.local/share/fish/local.fish"
-
-# Starship
-eval (starship init fish)
