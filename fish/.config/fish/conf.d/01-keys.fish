387d384648d83ba87bbb8c13d8dfc10539d2913b
Jake Bell <sigterm01@gmail.com>
2022-04-08 10:19:23 -0500

    Combining fish key bindings into one file

--- fish/.config/fish/conf.d/01-keys.fish
+++ fish/.config/fish/conf.d/01-keys.fish
@@ -1,1 +1,6 @@
+# https://github.com/theunraveler/zsh-fancy_ctrl_z
 bind \cZ 'fg'
+
+# https://github.com/oh-my-fish/plugin-bang-bang
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-  # https://github.com/oh-my-fish/plugin-bang-bang
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
