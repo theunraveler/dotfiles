f20c93ab8c22e00a08a0f3163dd6a1635a63c444
Jake Bell <sigterm01@gmail.com>
2022-04-11 09:52:43 -0500

    brew: Updating brews

---
+++ fish/.config/fish/conf.d/__activate_venv.fish
@@ -0,0 +1,23 @@
+function __activate_venv --on-variable PWD --description "Automatically activate python venv"
+  set -l venv_path $PWD/.venv
+
+  if not test -d $venv_path; return; end
+
+  set -l venv_hash (echo $venv_path | md5sum | awk '{print $1}')
+  set -l trust_path $HOME/.cache/trusted_venvs/$venv_hash
+
+  if test -f $trust_path
+    # Already trusted
+  else if read_confirm "Trust the virtualenv at $venv_path?"
+    mkdir -p (dirname $trust_path)
+    touch $trust_path
+  else
+    return
+  end
+
+  source $venv_path/bin/activate.fish
+  echo -n "Activated virtualenv at "
+  set_color --bold green
+  echo $venv_path
+  set_color normal
+end
