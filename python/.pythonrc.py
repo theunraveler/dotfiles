f262d6267bb1580c35bfb4d7c268e47641de2367
Jake Bell <sigterm01@gmail.com>
2022-06-01 12:43:09 -0500

    Automatically preload Django models in python shell

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -1,2 +1,12 @@
 # Preload some common modules for convenience.
 from pprint import pprint
+
+# Automatically load Django models.
+try:
+    import django
+except ImportError:
+    pass
+else:
+    for app in django.apps.apps.get_app_configs():
+        for model in app.get_models():
+            globals()[model.__name__] = model
