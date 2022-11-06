7ae520fa93d404841c6c9fe7ad95d27595ad8485
Jake Bell <sigterm01@gmail.com>
2022-11-06 16:02:43 -0600

    Simplifying `md2pdf` function

--- fish/.config/fish/functions/md2pdf.fish
+++ fish/.config/fish/functions/md2pdf.fish
@@ -1,5 +1,11 @@
 function md2pdf -d 'Convert markdown to PDF' -a filename
-  $MARKDOWN $filename > $filename.html &&
-  htmldoc --cont --bodyfont helvetica --textfont helvetica --headfootsize 8.0 --fontsize 10.0 --format pdf14 $filename.html > $filename.pdf &&
-  rm $filename.html
+  htmldoc \
+    --cont \
+    --bodyfont helvetica \
+    --textfont helvetica \
+    --headfootsize 8.0 \
+    --fontsize 10.0 \
+    --format pdf14 \
+    --footer '...' \
+    $filename > (path change-extension '' $filename).pdf
 end
