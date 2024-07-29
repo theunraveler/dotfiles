87cd676b3142d8966bad125f6eb60e1fb3acc68e
Jake Bell <sigterm01@gmail.com>
2024-07-29 15:49:50 -0500

    Adding a few more things to pythonrc

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -1,7 +1,8 @@
 # Preload some common modules for convenience.
 from pprint import pprint
+import re
 
-# Automatically load Django models.
+# Automatically load Django stuff.
 try:
     import django
 except ImportError:
@@ -10,3 +11,4 @@ else:
     for app in django.apps.apps.get_app_configs():
         for model in app.get_models():
             globals()[model.__name__] = model
+    from django.conf import settings
