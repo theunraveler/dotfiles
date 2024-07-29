e60e9410c9c7bf4f02472910c2a7b7374cb86fcd
Jake Bell <sigterm01@gmail.com>
2024-07-29 16:21:53 -0500

    Adding a few more things to pythonrc

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -1,6 +1,19 @@
+# flake8: noqa
+
 # Preload some common modules for convenience.
-from pprint import pprint
+import datetime
+import os
 import re
+import sys
+from pprint import pprint
+
+try:
+    from colors import green
+except ImportError:
+    green = str
+
+
+print('Imported the following items for your convenience: %s' % green('datetime, os, re, sys, pprint.pprint as pprint'))
 
 # Automatically load Django stuff.
 try:
@@ -8,7 +21,13 @@ try:
 except ImportError:
     pass
 else:
+    print('Imported %s' % green('django'))
+    models = []
     for app in django.apps.apps.get_app_configs():
         for model in app.get_models():
             globals()[model.__name__] = model
+            models.append(model.__name__)
+    print('Imported the following Django models for this app: %s' % green(', '.join(sorted(models))))
+
     from django.conf import settings
+    print('Imported Django settings as %s' % green('settings'))
